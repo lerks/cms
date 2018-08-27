96059e3d6c28905015752fee286698b0cc05746a
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2018-08-27 09:07:38 +0900

    Fixup

--- cmstestsuite/code/oom-heap.rs
+++ cmstestsuite/code/oom-heap.rs
@@ -2,12 +2,12 @@ use std::io;
 
 fn main()
 {
-    let mut v = vec![0; 128 * 1024 * 1024];
+    let mut v = vec![0; 256 * 1024 * 1024];
     let mut s = String::new();
     match io::stdin().read_line(&mut s)
     {
-        Ok(_) => v[10000] = s.trim().parse::<i32>().unwrap(),
+        Ok(_) => v[100000000] = s.trim().parse::<i32>().unwrap(),
         Err(why) => panic!("{}", why),
     };
-    println!("correct {}", v[10000]);
+    println!("correct {}", v[100000000]);
 }
